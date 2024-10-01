f0d9a8aefb516c267fd4e6851a4551851455ca5c
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2024-10-01 09:38:16 +0200

    01.10 final!

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [ playerName, setplayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+
+            onChangeName(symbol, playerName);
+
+        }
+        
     }
 
 
